de78045ee4aa89b1570cecba485919b1e55d437e
Michael Hosdez <hosdezm@gmail.com>
2022-05-03 07:18:50 +0200

    [Certificate] Small fixes and optimizations

--- src/Chamilo/Application/Weblcms/Tool/Implementation/Assessment/Component/RenderCertificateComponent.php
+++ src/Chamilo/Application/Weblcms/Tool/Implementation/Assessment/Component/RenderCertificateComponent.php
@@ -5,7 +5,6 @@ namespace Chamilo\Application\Weblcms\Tool\Implementation\Assessment\Component;
 use Chamilo\Application\Weblcms\Integration\Chamilo\Core\Tracking\Storage\DataClass\AssessmentAttempt;
 use Chamilo\Application\Weblcms\Tool\Implementation\Assessment\Manager;
 use Chamilo\Core\Repository\ContentObject\Assessment\Display\Attempt\AbstractAttempt;
-use Chamilo\Core\Repository\ContentObject\Assessment\Storage\DataClass\Assessment;
 use Chamilo\Core\Repository\Storage\DataClass\ContentObject;
 use Chamilo\Core\User\Storage\DataClass\User;
 use Chamilo\Libraries\Architecture\Application\ApplicationConfigurationInterface;
@@ -47,7 +46,7 @@ class RenderCertificateComponent extends Manager
         $userProperties = $this->getUserInfo($assessmentAttemptInfo[DataClass::PROPERTIES_DEFAULT][AbstractAttempt::PROPERTY_USER_ID]);
 
         // User properties
-        $properties['title'] = $assessmentInfo[DataClass::PROPERTIES_DEFAULT][Assessment::PROPERTY_TITLE];
+        $properties['title'] = $assessmentInfo[DataClass::PROPERTIES_DEFAULT][ContentObject::PROPERTY_TITLE];
         $properties['name'] = $userProperties[DataClass::PROPERTIES_DEFAULT][User::PROPERTY_FIRSTNAME] . ' ' . $userProperties[DataClass::PROPERTIES_DEFAULT][User::PROPERTY_LASTNAME];
         $properties['company'] = $userProperties[DataClass::PROPERTIES_DEFAULT][User::PROPERTY_OFFICIAL_CODE];
         $properties['score'] = $assessmentAttemptInfo[DataClass::PROPERTIES_DEFAULT][AbstractAttempt::PROPERTY_TOTAL_SCORE];
@@ -55,7 +54,7 @@ class RenderCertificateComponent extends Manager
             gmdate("l j F Y", $assessmentAttemptInfo[DataClass::PROPERTIES_DEFAULT][AbstractAttempt::PROPERTY_END_TIME])
         );
         $properties['valid'] = $date;
-        $properties['valid2'] = (int)(substr($date, strlen($date) - 4, 4)) + 1;
+        $properties['valid2'] = substr($date, 0, strlen($date) - 4) . ((int)(substr($date, strlen($date) - 4, 4)) + 1);
 
         return $this->getTwig()->render(
             'Chamilo\Application\Forelo:ForeloCertificate.html.twig', $properties);
@@ -80,7 +79,7 @@ class RenderCertificateComponent extends Manager
         $conditionItem = new EqualityCondition(
             new PropertyConditionVariable(
                 AssessmentAttempt::class_name(),
-                AssessmentAttempt::PROPERTY_ID),
+                DataClass::PROPERTY_ID),
             new StaticConditionVariable($uaid));
         $item = DataManager::retrieve(
             AssessmentAttempt::class_name(),
@@ -94,7 +93,7 @@ class RenderCertificateComponent extends Manager
         $conditionUser = new EqualityCondition(
             new PropertyConditionVariable(
                 User::class_name(),
-                User::PROPERTY_ID),
+                DataClass::PROPERTY_ID),
             new StaticConditionVariable($userId));
         $user = DataManager::retrieve(
             User::class_name(),
