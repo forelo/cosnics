a6a95dc2dde7cf6e69bec96aff875ca5a8d39746
Michael Hosdez <hosdezm@gmail.com>
2024-08-21 19:22:55 +0200

    [Certificate] Logo variable in template

--- src/Chamilo/Application/Weblcms/Tool/Implementation/Assessment/Component/RenderCertificateComponent.php
+++ src/Chamilo/Application/Weblcms/Tool/Implementation/Assessment/Component/RenderCertificateComponent.php
@@ -50,7 +50,8 @@ class RenderCertificateComponent extends Manager
                 $this->getConfigurationSetting('certificate_company') => $userProperties[DataClass::PROPERTIES_DEFAULT][User::PROPERTY_OFFICIAL_CODE],
                 $this->getConfigurationSetting('certificate_title') => $assessmentInfo[DataClass::PROPERTIES_DEFAULT][ContentObject::PROPERTY_TITLE],
                 $this->getConfigurationSetting('certificate_valid') => $date,
-                $this->getConfigurationSetting('certificate_validtill') => substr($date, 0, strlen($date) - 4) . ((int)(substr($date, strlen($date) - 4, 4)) + 1)
+                $this->getConfigurationSetting('certificate_validtill') => substr($date, 0, strlen($date) - 4) . ((int)(substr($date, strlen($date) - 4, 4)) + 1),
+                $this->getConfigurationSetting('certificate_logo') => strtolower($userProperties[DataClass::PROPERTIES_DEFAULT][User::PROPERTY_OFFICIAL_CODE])
             ]
         );
 
